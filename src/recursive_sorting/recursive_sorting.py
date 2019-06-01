9f11888ca60360d0fe2c07d3532f2703230aa361
Eddy <edward.gonzalez9708@gmail.com>
2019-06-01 18:21:29 -0400

    Completed quick sort problem and added quick sort tests to test_recursive.py

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,3 +1,37 @@
+import random
+
+# TO-DO: Implement the quick_sort() function in the Guided Project with your TA
+
+def quick_sort(arr):
+  if len(arr) < 2:
+    return arr
+
+  pivot = 0
+  ptr = len(arr) - 1
+  direction = -1
+
+  for x in range(0, len(arr)):
+    if direction == -1 and arr[pivot] > arr[ptr]:
+      arr[pivot], arr[ptr] = arr[ptr], arr[pivot]
+      pivot, ptr = ptr, pivot
+      direction = 1
+    elif direction == 1 and arr[pivot] < arr[ptr]:
+      arr[pivot], arr[ptr] = arr[ptr], arr[pivot]
+      pivot, ptr = ptr, pivot
+      direction = -1
+
+    ptr += direction
+    
+  return quick_sort(arr[0:pivot]) + [arr[pivot]] + quick_sort(arr[pivot + 1:])
+
+print('Quick Sort My Solution')
+print(quick_sort([1, 5, 8, 4, 2, 9, 6, 0, 3, 7]))
+print(quick_sort([]))
+print(quick_sort([2]))
+print(quick_sort([0, 1, 2, 3, 4, 5]))
+print(quick_sort(random.sample(range(200), 10)))
+print()
+
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
 def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
